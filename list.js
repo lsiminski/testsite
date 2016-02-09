82d8d08961355637e8e526a0114d2fa0bea46ca3
lsiminski <laurensiminski@gmail.com>
2016-02-09 13:04:47 -0500

    seriously?

--- list.js
+++ list.js
@@ -1,8 +1,10 @@
-$.getJSON('bg-images.json', function(data) { 
-
-  console.log(data.bgImages.length);    
-  var randomImage = data.bgImages[Math.floor(Math.random()*data.bgImages.length)];
-  console.log(randomImage);
-  $("body.random").css('background-image', randomImage.src);
-       
+$(document).ready(function() {
+	$.getJSON('bg-images.json', function(data) { 
+	
+	  console.log(data.bgImages.length);    
+	  var randomImage = data.bgImages[Math.floor(Math.random()*data.bgImages.length)];
+	  console.log(randomImage);
+	  $("body.random").css('background-image', randomImage.src);
+	       
+	});
 });

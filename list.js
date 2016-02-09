cc2cada8b0aa70807e3584db109d52dedfe78b73
lsiminski <laurensiminski@gmail.com>
2016-02-09 12:56:39 -0500

    sdfsdf

--- list.js
+++ list.js
@@ -1,18 +1,6 @@
-$.getJSON('list.json', function(data) { 
-
-  var d = new Date();
-  var mo = d.getMonth();
-  var da = d.getDate ();
-  var moda = mo + 1 +"-" + da;
+$.getJSON('bg-images.json', function(data) { 
     
-  $.each(data, function(data) {
-	  if ( this.date === moda ) {
-		  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>");
-		  $("#list_day p").text(this.day);
-		  $("#list_category p").text(this.category);
-		  $("body.random").css("background-image","url(" + this.image + ")");
-	  } else {
-	  }
-  });
-    
+  var randomImage = data.bgImages[Math.floor(Math.random()*data.bgImages.length)];
+  $("body").css('background-image', randomImage.src);
+       
 });

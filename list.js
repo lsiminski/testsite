85b4e909220bbd9ccf6bf073e2067e6190708482
lsiminski <laurensiminski@gmail.com>
2016-02-09 13:03:58 -0500

    okay, maybe this will work

--- list.js
+++ list.js
@@ -1,7 +1,8 @@
 $.getJSON('bg-images.json', function(data) { 
-    
+
+  console.log(data.bgImages.length);    
   var randomImage = data.bgImages[Math.floor(Math.random()*data.bgImages.length)];
   console.log(randomImage);
-  $("body").css('background-image', randomImage.src);
+  $("body.random").css('background-image', randomImage.src);
        
 });

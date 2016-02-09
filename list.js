9c5cc0ee2cb2971f8e132c0a649a70e58fef0d0a
lsiminski <laurensiminski@gmail.com>
2016-02-09 12:59:46 -0500

    so, did you update?

--- list.js
+++ list.js
@@ -1,6 +1,7 @@
 $.getJSON('bg-images.json', function(data) { 
     
   var randomImage = data.bgImages[Math.floor(Math.random()*data.bgImages.length)];
+  console.log(randomImage);
   $("body").css('background-image', randomImage.src);
        
 });
